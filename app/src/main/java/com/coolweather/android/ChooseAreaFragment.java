dd75dd69abfc559ff252e1646bd8b8d177f1c4e2
lcc020499 <lcc020499@github.com>
2020-04-08 05:48:33 +0000

    Add Bing bcakground

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.coolweather.android;
 
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -137,9 +138,27 @@ public class ChooseAreaFragment extends Fragment {
 
                     queryCounties();
 
+
+                }else if(currentLevel==LEVEL_COUNTY){
+
+                    String weatherId = countyList.get(position).getWeatherId();
+
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+
+                    intent.putExtra("weather_id",weatherId); // 向intent传入WeatherId
+
+                    startActivity(intent);
+
+                    getActivity().finish();
+
                 }
 
             }
+
+
+
+
+
 
         });
 
